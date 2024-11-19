0eb11aa81514de4c3a1cb185c3d1ba4c17d45cd4
NhanDev <huynhthanhnhan632004@gmail.com>
2024-11-19 15:41:52 +0700

    update code 25 ngay 19/11/2024

--- script.js
+++ script.js
@@ -33,5 +33,20 @@ var tips = [
     Calculator(bill[1]),
     Calculator(bill[2]),
 ];
-console.log(tips);
+// console.log(tips);
 console.log("-----------------------------------------------------------------");
+var john = {
+    firstName: "John",
+    lastName: "Smith",
+    birthYear: 2024,
+    family: ['Quin', "Tri", "Bob", "Emily"],
+    job: "teacher",
+    isMarried: false,
+    tinhtoan: function (birthDay) {
+        // return 2024 - this.birthYear;
+        return 2024 - birthDay;
+    }
+};
+console.log(john.tinhtoan(2004));
+// console.log("Lấy value từ key", john.birthYear); // Lấy value từ key
+// console.log("Tính toán hàm", john.tinhtoan(2004));
