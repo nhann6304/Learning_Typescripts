deea4457df3329dcbb4d57236a5a32dba0850fb6
NhanDev <huynhthanhnhan632004@gmail.com>
2024-11-22 14:08:17 +0700

    update and start leaning express

---
+++ nodejs/node.ts
@@ -0,0 +1,43 @@
+// import testFn from "./src/test";
+// import path from "path"
+// import http from "http"
+// import { readFileSync, writeFileSync } from "fs"
+
+// {
+//     //path
+//     const first = readFileSync("./nodejs/src/test.txt", "utf-8")
+
+
+//     console.log(testFn<string>(first));
+
+
+//     writeFileSync("./nodejs/src/vietra.txt", "co gan len nha ");
+// }
+
+// {
+//     //http
+//     // request => yêu cầu từ client gửi xuống
+//     // response => yêu cầu từ server gửi lên
+//     // const server = http.createServer((request, response) => {
+//     //     console.log("chạy sau thằng listen");
+//     //     if (request.url === "/") {
+//     //         response.end("Home page nè")
+//     //     } else if (request.url === "/about") {
+//     //         response.end("about page nè")
+//     //     } else {
+//     //         response.end(`<h1>404</h1>`)
+//     //     }
+//     // })
+//     // server.listen(8001, () => {
+//     //     console.log("server is running port 8001");
+//     // })
+// }
+console.log("-------------------------------------------------------------------------------");
+//express 
+
+
+
+
+console.log("-------------------------------------------------------------------------------");
+
+
