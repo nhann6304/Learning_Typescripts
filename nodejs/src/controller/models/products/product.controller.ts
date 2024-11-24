efb72ac204bc10b67161be9b276f44b4653cfe84
NhanDev <huynhthanhnhan632004@gmail.com>
2024-11-24 20:07:30 +0700

    commitMessage

--- nodejs/src/controller/models/products/product.controller.ts
+++ nodejs/src/controller/models/products/product.controller.ts
@@ -1,9 +1,10 @@
-import mongoose from "mongoose"
-import { IProduct } from "../../../interface/models/product.interface"
-import { productSchema } from "../../../models/models/product.schema"
-import { CREATE } from "../../../core/success.response";
-import data from "../../../data/product.data.json"
+import mongoose from "mongoose";
+import { IProduct } from "../../../interface/models/product.interface";
+import { productSchema } from "../../../models/models/product.schema";
+import { CREATE, OK } from "../../../core/success.response";
+import data from "../../../data/product.data.json";
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
 const productRepository = mongoose.model<IProduct>("products", productSchema);
 
 export const mockupData = async (req: Request, res: Response) => {
@@ -11,4 +12,44 @@ export const mockupData = async (req: Request, res: Response) => {
         message: "Mockup data success!!!",
         metadata: await productRepository.create(data),
     }).send(res);
-}
+};
+
+export const mockupDelete = async (req: Request, res: Response) => {
+    new OK({
+        message: "Mockup data success!!!",
+        metadata: await productRepository.deleteMany(),
+    }).send(res);
+};
+
+export const findProduct = async (req: Request, res: Response) => {
+    const { id, name, fields, limit = 10, page = 1, ...query } = req.query;
+    console.log(fields);
+    const a = fields as string;
+    const fieldsList = a?.split(",").join(" ");
+    //pagination
+    const skip = (+page - 1) * Number(limit)
+    console.log("skip::::", skip);
+    let result = [];
+    try {
+        if (id) {
+            result = (await productRepository
+                .findOne({ _id: id, name: { $regex: name, $options: "i" }, ...query })
+                .select(`${fieldsList}`)) as any;
+        } else {
+            result = await productRepository
+                .find({ ...query })
+                .select(`${fieldsList}`)
+                .limit(+limit)
+                .skip(skip);
+        }
+        new OK({
+            message: "Find Product Success",
+            metadata: result,
+        }).send(res);
+    } catch (error) {
+        new OK({
+            message: "Find Product Failed",
+            statusCode: StatusCodes.INTERNAL_SERVER_ERROR,
+        }).send(res);
+    }
+};
--- nodejs/src/routes/models/product.routes.ts
+++ nodejs/src/routes/models/product.routes.ts
@@ -2,8 +2,9 @@ import express from "express";
 import { auth } from "../../middleware/auth.middleware";
 import { ProductController } from "../../controller/models/products";
 
-
 const router = express.Router();
 router.use(auth);
-router.route("/mockup").post(ProductController.mockupData)
+router.route("/mockup/create").post(ProductController.mockupData)
+router.route("/mockup/delete").delete(ProductController.mockupDelete)
+router.route("/find").get(ProductController.findProduct)
 export default router
